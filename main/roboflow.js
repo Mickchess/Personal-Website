4a1950f5bd95e527f46809649058367cd382b140
repairedbirch41 <121144256+repairedbirch41@users.noreply.github.com>
2025-01-16 19:08:08 -0800

    vercel

---
+++ main/roboflow.js
@@ -0,0 +1,14 @@
+import { InferenceEngine } from "inferencejs";
+
+document.getElementById('uploadForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const inferEngine = new InferenceEngine();
+    const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4/2", "1", "rf_tL1P1cdIQmUGii8baelMMhWAT3E3");
+
+    const image = document.getElementById("itemImage").files[0]; // Get the uploaded file
+    const cvimg = new CVImage(image);
+    const predictions = await inferEngine.infer(workerId, cvimg); // Infer on image
+
+    console.log(predictions); // Log the predictions
+});
