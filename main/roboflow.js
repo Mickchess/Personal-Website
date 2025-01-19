9a3ed233c3dfe20815247a0aea31d9b6cf474f19
Mickchess <136009342+Mickchess@users.noreply.github.com>
2025-01-18 22:37:14 -0800

    cdw

--- main/roboflow.js
+++ main/roboflow.js
@@ -1,14 +1,8 @@
 const inferencejs = window.inferencejs
 const inferEngine = new inferencejs.InferenceEngine();
 
-document.getElementById('uploadForm').addEventListener('submit', async function(event) {
-    event.preventDefault(); // Prevent the default form submission
-    const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4/2", "2", "z8MimQV5Bi7a9IrpJYhE");
-
-    const image = document.getElementById("itemImage").files[0]; // Get the uploaded file
-    const cvimg = new CVImage(image);
-    const predictions = await inferEngine.infer(workerId, cvimg); // Infer on image
-
-    console.log(predictions); // Log the predictions
-});
-
+const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4/2", "2", "rf_AZWB6RdKlqJ8x5gXmulz");
+const image = document.getElementById("itemImage").files[0]; // Get the uploaded file
+const cvimg = new CVImage(image);
+const predictions = await inferEngine.infer(workerId, cvimg); // Infer on image
+console.log(predictions); // Log the predictions
