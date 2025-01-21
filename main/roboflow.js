9343db53f996f7c1630a15d01488324c8c750b92
repairedbirch41 <121144256+repairedbirch41@users.noreply.github.com>
2025-01-20 18:51:42 -0800

    fix: update roboflow.js

--- main/roboflow.js
+++ main/roboflow.js
@@ -3,7 +3,7 @@ const inferEngine = new InferenceEngine();
 
 document.getElementById('uploadForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent the default form submission
-    const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4/2", "1", "rf_tL1P1cdIQmUGii8baelMMhWAT3E3");
+    const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4", "2", "rf_tL1P1cdIQmUGii8baelMMhWAT3E3");
 
     const image = document.getElementById("itemImage").files[0]; // Get the uploaded file
     const cvimg = new CVImage(image);
