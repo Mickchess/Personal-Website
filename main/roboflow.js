88bd9c13e85a862dd3155afb025c2e031d082e25
micklam101 <micklam101@hotmail.com>
2025-01-20 22:41:15 -0800

    pls

--- main/roboflow.js
+++ main/roboflow.js
@@ -4,13 +4,6 @@ const CVImage = inferencejs.CVImage;
 
 document.getElementById('uploadForm').addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent the default form submission
-    const workerId = await inferEngine.startWorker("science-fair-7r503m-ab7q4", "2", "rf_tL1P1cdIQmUGii8baelMMhWAT3E3");
-
-    const image = document.getElementById("itemImage").files[0]; // Get the uploaded file
-    const cvimg = new CVImage(image);
-    const predictions = await inferEngine.infer(workerId, cvimg); // Infer on image
-
-    console.log(predictions); // Log the predictions
 
     // Display the uploaded image
     const reader = new FileReader();
