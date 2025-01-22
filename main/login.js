568787cdceae96840aaf974b4a878bc3c3dc8d33
MickChess <micklam101@gmail.com>
2025-01-22 12:23:52 -0800

    Implement login functionality with localStorage and update UI elements

--- main/login.js
+++ main/login.js
@@ -1,2 +1,25 @@
-document.getElementById('login').addEventListener('submit', function() {
+document.addEventListener('DOMContentLoaded', function() {  
+
+    if (localStorage.getItem('username') == null) {
+        localStorage.setItem('username', 'mryuill');
+        localStorage.setItem('password', 'pizza');
+    }
+});
+
+
+
+
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    
+    if (username == localStorage.getItem('username') && password == localStorage.getItem('password')) {
+        window.location.href = 'app.html';
+    }
+    else {
+        alert('Invalid username or password!');
+    }
 });
