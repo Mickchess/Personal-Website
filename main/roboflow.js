98da566b087ff969a63224a40dff57b77ac8df99
repairedbirch41 <121144256+repairedbirch41@users.noreply.github.com>
2025-02-26 20:51:02 -0800

    fix: update API endpoint and key for food detection workflow

--- main/roboflow.js
+++ main/roboflow.js
@@ -14,13 +14,13 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
 
         // Define and call the infer function
         var infer = async function() {
-            const response = await fetch('https://detect.roboflow.com/infer/workflows/food-detection-rkdfg/custom-workflow', {
+            const response = await fetch('https://detect.roboflow.com/infer/workflows/free-trial-lol/foodfinder', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    api_key: 'U5PVZEUpXhY5PMNHobWA',
+                    api_key: 'FOtktWh0YseeBvn4A4WJ',
                     inputs: {
                         "image": {"type": "base64", "value": base64String}
                     }
